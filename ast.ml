645b3c849f2987f19bccc4d74dd5738e69ff0fbf
chrisco210 <chrisrachlinski@gmail.com>
2020-11-12 17:07:02 -0500

    Define AST for lml

--- ast.ml
+++ ast.ml
@@ -3,16 +3,39 @@
 type var = string
 
 type com = 
-  | Let of com * var * com
+  (* Regular functional stuff *)
+  | Let of var * com * com
+  | Letg of var * com
+  | If of com * com * com
   | Tuple of com * com
+  | Fun of (var list) * com
+  | Proj of com * int
+
+  (* Operations *)
   | Bop of com * bop * com
   | Uop of uop * com
-  | Fun of var * com
-  | Proj of com * int
+
+  (* Imperative features *)
+  | Seq of com * com
+  | Ref of com 
+  | While of com * com
+  | Assign of com * com
+  | Break
+  | Continue
+
+  (* Lambda calculus *)
+  | Abs of var * com
+  | App of com * com
+
+  (* List operations *)
   | Hd of com
   | Tl of com
+  | Nil
+
+  (* Literals *)
   | Var of var
   | Int of int
+  | Bool of bool
 and bop = 
   | Plus 
   | Minus
@@ -23,6 +46,8 @@ and bop =
   | Gteq
   | Gt
   | Neq
+  | Cons
 and uop =
-  | Not of com
-  | Neg of com
+  | Not
+  | Neg 
+  | Deref
