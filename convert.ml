d3f48942a964ff29bf6ac66e2bd423b1fa06da07
chrisco210 <chrisrachlinski@gmail.com>
2020-12-05 19:32:44 -0500

    Rework cps translation, basic things are working

--- convert.ml
+++ convert.ml
@@ -2,26 +2,20 @@
 
 open Ast
 open Lambdaast
+open Iast
 open Convertcps
 open Church
 
 (* L x . L y . L z . z x y *)
-let pair = Lam (Lam (Lam (App (App (Var 0, Var 2), Var 1))))
+let pair = Lam ("***x", Lam ("***y" ,Lam ("***z", App (App (Var "***z", Var "***x"), Var "***y"))))
 
 (* L p . p (L x . L y . x) *)
-let fst = Lam (App (Var 0, Lam (Lam (Var 1))))
+let fst = Lam ("***p", App (Var "***p", Lam ("***x", Lam ("***y", Var "***x"))))
 
 (* L p . p (L x . L y . y) *)
-let snd = Lam (App (Var 0, Lam (Lam (Var 0))))
+let snd = Lam ("***p", App (Var "***p", Lam ("***x", Lam ("***y", Var "***y"))))
 
-let list_posn (lst : 'a list) (item : 'a) : int option = 
-  let rec list_posni (lst : 'a list) (item : 'a) (n : int) : int option = 
-    match lst with
-    | h::t when h = item -> Some n
-    | h::t -> list_posni t item (n + 1)
-    | [] -> None
-  in 
-  list_posni lst item 0
+
 
 (** [lambop_of_bop b] is the lambdaast version of a binary operator*)
 let lambop_of_bop (b : Ast.bop) : Lambdaast.bop = 
@@ -46,68 +40,64 @@ let lambop_of_uop (u : Ast.uop) : Lambdaast.uop =
   | Tl -> failwith "Tl cannot be converted directly"
 
 (** [convert e] is a lambda calculus translation of an expression e*)
-let rec convert_var (e : expr) (s : Ast.var list) : iast = 
+let rec convert_var (e : expr) : iast = 
   match e with
-  | Abs(v,e') -> Lam (convert_var e' (v::s))
-  | App(e1,e2) -> App (convert_var e1 s, convert_var e2 s)
-  | Let(v,e1,e2) -> convert_var (App (Abs (v, e2), e1)) s
+  | Abs(v,e') -> Lam (v, convert_var e')
+  | App(e1,e2) -> App (convert_var e1, convert_var e2)
+  | Let(v,e1,e2) -> (App (Lam (v, convert_var e2), convert_var e1)) 
   (* Uses the Y combinator for recursion *)
-  | Letrec (v, va, e, b) -> let ycomb = Lam (
-      App (
-        (Lam (App (Var 1, App (Var 0, Var 0)))), 
-        (Lam (App (Var 1, App (Var 0, Var 0))))
-      )
-    ) in 
-    let converted = convert_var (Abs (v, Fun (va, e))) s in 
-    let convbody = convert_var (Abs (v, b)) s in
+  | Letrec (v, va, e, b) -> 
+    let ycomb = Lam ("**f", 
+                     App (
+                       (Lam ("**x", App (Var "**f", App (Var "**x", Var "**x")))), 
+                       (Lam ("**x", App (Var "**f", App (Var "**x", Var "**x"))))
+                     )
+                    ) in 
+    let converted = convert_var (Abs (v, Fun (va, e)))  in 
+    let convbody = convert_var (Abs (v, b))  in
     App (convbody, App (ycomb, converted))
-  | If(e1,e2,e3) -> If (convert_var e1 s, convert_var e2 s, convert_var e3 s)
-  | Var(v) -> begin
-      match list_posn s v with
-      | Some n -> Var n
-      | None -> failwith "Unbound variables cannot be converted"
-    end
+  | If(e1,e2,e3) -> If (convert_var e1, convert_var e2, convert_var e3)
+  | Var(v) -> Var v
   | Int(n) -> Int n
   | Bool(b) -> Bool b
   | Bop(e1,b,e2) -> begin
       match b with
-      | Cons -> App (App (pair,  Bool false), App (App (pair, convert_var e1 s), convert_var e2 s))
-      | And -> If (convert_var e1 s, convert_var e2 s, Bool false)
-      | Or -> If (convert_var e1 s, Bool true, convert_var e2 s)
-      | _ -> Bop (lambop_of_bop b, convert_var e1 s, convert_var e2 s)
+      | Cons -> App (App (pair,  Bool false), App (App (pair, convert_var e1), convert_var e2))
+      | And -> If (convert_var e1, convert_var e2, Bool false)
+      | Or -> If (convert_var e1, Bool true, convert_var e2)
+      | _ -> Bop (lambop_of_bop b, convert_var e1, convert_var e2)
     end
-  | Fun(v,e') -> List.fold_right 
-                   (fun elt acc -> Lam acc) 
-                   v 
-                   (convert_var e' ((List.rev v) @ s))
+  | Fun(vs,e') -> 
+    let converted_body = convert_var e' in
+    List.fold_right (fun v acc -> Lam (v, acc)) vs converted_body
   | Uop(u,e') -> begin
       match u with
-      | Hd -> let x = convert_var e' s in begin match x with
+      | Hd -> let x = convert_var e'  in begin match x with
           | App (App (_,  Bool b), e2) when b = false -> App (fst, e2)
           | App (App (_,  Bool b), e2) -> x
           | _ -> failwith "Mismatched type for hd operation"
         end
-      | Tl -> let x = convert_var e' s in begin match x with
+      | Tl -> let x = convert_var e'  in begin match x with
           | App (App (_,  Bool b), e2) when b = false -> App (snd, e2)
           | App (App (_,  Bool b), e2) -> x
           | _ -> failwith "Mismatched type for tl operation"
         end
       | Deref -> failwith "Unimplemented in gamma"
-      | _ -> Uop (lambop_of_uop u, convert_var e' s)
+      | _ -> Uop (lambop_of_uop u, convert_var e')
     end
   (* Everything below here is unimplemented for alpha *)
   | Letg(v,e') -> failwith "unimplemented in beta"
   (* Using the encoding from g from 
      https://en.wikipedia.org/wiki/Church_encoding#Church_pairs *)
   | Tuple(e1,e2) -> 
-    App (App (pair,  (convert_var e1 s)), convert_var e2 s)
+    App (App (pair,  (convert_var e1)), convert_var e2)
   | Proj(e',n) -> let rec proj_rec (en : iast) (n : int) : iast =  
                     match n with
                     | 0 -> (App (fst, en))
                     | 1 -> (App (snd, en))
                     | _ -> failwith "n-ary tuples are not implemented"
-    in proj_rec (convert_var e' s) n
-  | Seq(e1,e2) -> Seq(convert_var e1 s, convert_var e2 s)
+    in proj_rec (convert_var e') n
+  | Seq(e1,e2) -> Seq(convert_var e1, convert_var e2)
   | Ref(e') -> failwith "unimplemented in gamma"
   | While(e1,e2) -> failwith "unimplemented in gamma"
   | Assign(e1,e2) -> failwith "unimplemented in gamma"
@@ -117,5 +107,5 @@ let rec convert_var (e : expr) (s : Ast.var list) : iast =
 
 
 let rec convert (e : expr) : lamcom = 
-  let translated = convert_var e [] |> convert_cps in 
+  let translated = convert_var e |> convert_cps in 
   App (translated, Lam (Var 0))
--- convertcps.ml
+++ convertcps.ml
@@ -1,85 +1,119 @@
 open Lambdaast
+open Iast
 
-(* The type of an intermediate ast with features requiring continuations *)
-type iast = 
-  | App of iast * iast
-  | Lam of iast
-  | Var of var
-  | Int of int
-  | Bool of bool
-  | Bop of Lambdaast.bop * iast * iast
-  | Uop of Lambdaast.uop * iast
-  | If of iast * iast * iast
-  (* Imperative features that are not converted at this stage *)
-  | Seq of iast * iast
-  | Ref of iast 
-  | While of iast * iast
-  | Assign of iast * iast
-  | Break
-  | Continue
+let counter = ref 0
 
-(** [raise_abs_var e tr] raises variables inside of abstractions so they still
-    point to the correct lambda level  *)
-let rec raise_abs_var (e : iast) (tr : int) : iast =
+(** [free_var] generates a fresh variable that has not been used before.  
+    It will never match a user defined variable because it includes characters
+    that cannot be included in user defined variable names. *)
+let free_var () : ivar = 
+  counter := !counter + 1; 
+  "*/" ^ (string_of_int !counter) 
+
+let rec convert_cps_init (e : iast) : iast = 
+  match e with
+  | App (e1, e2) -> 
+    let k = free_var () in
+    let f = free_var () in
+    let v = free_var () in
+    Lam (k, 
+         App (
+           convert_cps_init e1, 
+           Lam (
+             f, 
+             App (
+               convert_cps_init e2,
+               (Lam (v, App (App (Var f, Var v), Var k)))
+             )
+           )
+         )
+        )
+  | Lam (v, e') -> 
+    let k = free_var () in
+    let k' = free_var () in
+    Lam (k, 
+         App (Var k, 
+              Lam (v,
+                   Lam (k',
+                        App (
+                          convert_cps_init e',
+                          Var k'
+                        )
+                       )
+                  )
+             )
+        )
+  | Bop (b, e1, e2) -> 
+    let k = free_var () in
+    let n = free_var () in 
+    let m = free_var () in
+    Lam (k, 
+         App (
+           convert_cps_init e1,
+           Lam (n,
+                App (
+                  convert_cps_init e2,
+                  Lam (m, 
+                       App (
+                         Var k,
+                         Bop (b, Var n, Var m)
+                       )
+                      )
+                )
+               )
+         )
+        )
+  | Uop (u, e') -> failwith "Unimplemented convert_cps_init"
+  | If (g, e1, e2) -> failwith "Unimplemented convert_cps_init"
+  | Seq (e1, e2) -> failwith "Unimplemented convert_cps_init"
+  | Ref (e) -> failwith "Unimplemented convert_cps_init"
+  | While (e1, e2) -> failwith "Unimplemented convert_cps_init"
+  | Assign (e1, e2) -> failwith "Unimplemented convert_cps_init"
+  | Break -> failwith "Unimplemented convert_cps_init"
+  | Continue -> failwith "Unimplemented convert_cps_init"
+  | Var v -> 
+    let k = free_var () in 
+    Lam (k, App (Var k, Var v))
+  | Int i -> 
+    let k = free_var () in 
+    Lam (k, App (Var k, Int i))
+  | Bool b -> 
+    let k = free_var () in 
+    Lam (k, App (Var k, Bool b))
+
+let rec convert_cps_vars (s : ivar list) (e : iast) : lamcom = 
+  let list_posn (lst : 'a list) (item : 'a) : int option = 
+    let rec list_posni (lst : 'a list) (item : 'a) (n : int) : int option = 
+      match lst with
+      | h::t when h = item -> Some n
+      | h::t -> list_posni t item (n + 1)
+      | [] -> None
+    in 
+    list_posni lst item 0
+  in
   match e with 
-  | Var v -> if v = tr then Var (1 + 3 * v) else Var v
-  | Lam e' -> Lam (raise_abs_var e' (tr + 1)) 
-  | App (e1, e2) -> App (raise_abs_var e1 tr, raise_abs_var e2 tr)
+  | App (e1, e2) -> App (convert_cps_vars s e1, convert_cps_vars s e2)
+  | Lam (v, e') -> Lam (convert_cps_vars (v::s) e')
+  | Bop (b, e1, e2) -> Bop (b, convert_cps_vars s e1, convert_cps_vars s e2)
+  | Uop (u, e') -> Uop (u, convert_cps_vars s e')
+  | If (g, e1, e2) -> If (convert_cps_vars s g,
+                          convert_cps_vars s e1,
+                          convert_cps_vars s e2)
+  | Seq (e1, e2) -> failwith "Cannot directly convert a Seq"
+  | Ref (e) -> failwith "Cannot directly convert a Ref"
+  | While (e1, e2) -> failwith "Cannot directly convert a While"
+  | Assign (e1, e2) -> failwith "Cannot directly convert a Assign"
+  | Break -> failwith "Cannot directly convert a Break"
+  | Continue -> failwith "Cannot directly convert a Continue"
+  | Var v -> begin 
+      match (list_posn s v) with 
+      | Some i -> Var i
+      | None -> failwith ("Unbound variable: " ^ v)
+    end
   | Int i -> Int i
   | Bool b -> Bool b
-  | If (e1, e2, e3) -> If(raise_abs_var e1 tr, raise_abs_var e2 tr, raise_abs_var e3 tr)
-  | Bop (b, e1, e2) -> Bop (b, raise_abs_var e1 tr, raise_abs_var e2 tr)
-  | Uop (b, e') -> Uop (b, raise_abs_var e' tr)
-  | Seq (e1, e2) -> Seq (raise_abs_var e1 tr, raise_abs_var e2 tr)
-  | _ -> failwith "Unimplemented raise_abs_var"
 
 (** [convert_cps e] converts an intermediate ast into a lambda expression in 
     continuation passing style.*)
 let rec convert_cps (e : iast) : lamcom =
-  match e with
-  | App (e1, e2) -> 
-    Lam (
-      App (
-        convert_cps e1,
-        Lam (
-          App (
-            convert_cps e2,
-            Lam (
-              App (
-                App (
-                  Var 1,
-                  Var 0
-                ),
-                Var 2
-              )
-            )
-          )
-        )
-      )
-    )
-  | Lam e' -> Lam (
-      App (
-        Var 0,
-        Lam (
-          Lam (
-            App (
-              convert_cps (raise_abs_var e' 0),
-              Var 0
-            )
-          )
-        )
-      )
-    )
-  | Var v -> Lam (App (Var 0, Var (v + 1)))
-  | Int i -> Lam (App (Var 0, Int i))
-  | Bool b -> Lam (App (Var 0, Bool b))
-  | Bop (b, e1, e2) -> failwith "Unimplemented CPS translation"
-  | Uop (u, e) -> failwith "Unimplemented CPS translation"
-  | If (b, e1, e2) -> failwith "Unimplemented CPS translation"
-  (* Imperative features *)
-  | Seq (e1, e2) -> failwith "Unimplemented CPS translation"
-  | Ref (e) -> failwith "Unimplemented CPS translation"
-  | While (e1, e2) -> failwith "Unimplemented CPS translation"
-  | Assign (e1, e2) -> failwith "Unimplemented CPS translation"
-  | Break -> failwith "Unimplemented CPS translation"
-  | Continue -> failwith "Unimplemented CPS translation"
+  e |> convert_cps_init |> convert_cps_vars []
