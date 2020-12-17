000503746b161c304419fb61f0732e6a0645004d
Aidan Campbell <aec294@cornell.edu>
2020-12-17 15:46:50 -0500

    UI improvements sans arrow capabilities

--- repl.ml
+++ repl.ml
@@ -30,26 +30,72 @@ module Make (C : Converter) : Repl = struct
   open Interp
 
   let eval_and_print (e : expr) : unit = let e' = C.convert e in 
-    print_endline ("-> " ^ (string_of_exp e'));
-    print_endline ("-> " ^ (e' |> eval |> string_of_exp))
+    ANSITerminal.(print_string [green] ("-> "));
+    ANSITerminal.(print_string [white] ((string_of_exp e') ^ "\n"));
+    ANSITerminal.(print_string [green] ("-> "));
+    ANSITerminal.(print_string [white] ((e' |> eval |> string_of_exp) ^ "\n"))
+
+  let help () =
+    ANSITerminal.(print_string [yellow] ("
+The ML language is defined with the following grammar:
+
+bop ::= + | - | * | < | > | <= | >= | = | != | ::
+uop ::= ~ | ~- | !
+
+e ::= (e)
+
+      | let x = e1 in e2
+      | let rec x = e1 in e2
+      | fun x1 ... xn -> e
+      | e1 e2
+      | if e1 then e2 else e3
+      | while e1 do e2 done
+      | e1; e2
+      | ref e
+      | e1 := e2
+      | x
+      | []                   <-- Nil
+      | L x . e
+      | let x = e
+      | (e1, e2)
+      | e#n
+      | e1 bop e2
+      | uop e
+      | n
+      | ()                   <-- Unit
+      | true
+      | false
+
+These generally do what you would expect them to do.  The `::` operator is a 
+cons operator to append to a list, `e#n` extracts the `n`th item from a tuple
+`e`. For unary operators,  `~` is logical negation, while `~-` is integer 
+negation, and `!` is dereferencing.
+
+You can do OCaml style comments using the `(* ocaml comment *)` syntax.\n"))
 
   let rec eval_and_loop () = 
-    print_endline "--------------------------------------------------------------------------------";
-    print_string ">  ";
+    ANSITerminal.(print_string [cyan] ("---------------------------------------------------------------------------------------\n"));
+    ANSITerminal.(print_string [green] (">  "));
     (* Exception handling based on https://ocaml.org/learn/tutorials/error_handling.html  *)
     let input = read_line () in
     begin
-      try input |> parse |> eval_and_print with
-        e -> let msg = Printexc.to_string e
-        and stack = Printexc.get_backtrace () in
-        print_endline ("Error evaluating or parsing: " ^ msg ^ stack)
+      match input with
+      | "help" -> help ()
+      | _ -> 
+        begin
+          try input |> parse |> eval_and_print with
+            e -> let msg = Printexc.to_string e
+            and stack = Printexc.get_backtrace () in
+            ANSITerminal.(print_string [red] ("Error evaluating or parsing: " ^ msg ^ stack ^ "\n"))
+        end
     end;
     eval_and_loop ()
 
   let repl () = 
-    print_endline "--------------------------------------------------------------------------------";
-    print_endline "                                    LML REPL                                    ";
-    print_endline "--------------------------------------------------------------------------------";
-    print_endline "Press ^C to quit, see README.md for syntax help and some examples to run";
+    ANSITerminal.(print_string [cyan] ("
+---------------------------------------------------------------------------------------
+                                        LML REPL                                    
+---------------------------------------------------------------------------------------
+Press ^C to quit, see README.md or type `help` for syntax help and some examples to run\n"));
     eval_and_loop ()
 end
